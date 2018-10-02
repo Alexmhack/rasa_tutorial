1eea0584bd4122d9c72b17b441c818773c84e4b1
alexHooper <alexmohack@gmail.com>
2018-10-02 20:27:21 +0530

    add interpreter in agent

--- rasa_core/core_model.py
+++ rasa_core/core_model.py
@@ -1,22 +1,27 @@
+import time
+
 from rasa_core.agent import Agent
 
 messages = ['Hi! I am Django Services Bot. You can enter "stop" anytime to end the conversation']
 
-agent = Agent.load('models/dialogue')
+agent = Agent.load('models/dialogue', interpreter='models/current/nlu')
 
-def chatlogs_html(messages):
-	messages_html = "".join(["<p>{}</p>".format(m) for m in messages])
-	chatbot_html = "<div id='chatbotMessage'>{}</div>".format(messages_html)
-	return chatbot_html
+def chatlogs_html(message):
+	messages_html = f"<p>{message}</p>"
+	# chatbot_html = "<div id='chatbotMessage'>{}</div>".format(messages_html)
+	return messages_html
 
 
 while True:
-	print(chatlogs_html(messages))
-	time.sleep(0.3)
-	a = input()
+	print([m for m in messages])
+	a = input(">> ")
 	messages.append(a)
 	if a == 'stop':
 		break
 	responses = agent.handle_message(a)
+	print(f"RESPONSE: {responses}")
 	for r in responses:
+		print(r)
 		messages.append(r.get('text'))
+	time.sleep(0.3)
+	print(chatlogs_html(a))
