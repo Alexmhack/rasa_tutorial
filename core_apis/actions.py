86e81fa4cb6dad25f1ca3f657da9867e82f0bd0c
alexHooper <alexmohack@gmail.com>
2018-10-05 18:35:01 +0530

    define action for send email

--- core_apis/actions.py
+++ core_apis/actions.py
@@ -1,2 +1,10 @@
 from rasa_core_sdk import Action
 from rasa_core_sdk.events import SlotSet
+
+class ActionSendEmail(Action):
+	def name(self):
+		return "action_send_email"
+
+	def run(self, dispatcher, tracker, domain):
+		email_id = tracker.get_slot('email_id')
+		return f"Confirmation email is sent to {email_id}"
