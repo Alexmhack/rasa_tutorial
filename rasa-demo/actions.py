1f0d168bd4407748448b41befb368d15989b7564
alexHooper <alexmohack@gmail.com>
2018-10-11 22:29:11 +0530

    file for custom actions

---
+++ rasa-demo/actions.py
@@ -0,0 +1,17 @@
+class ActionStoreEmail(Action):
+    """Stores the email in a slot"""
+
+    def name(self):
+        return "action_store_email"
+
+    def run(self, dispatcher, tracker, domain):
+        email = next(tracker.get_latest_entity_values('email'), None)
+
+        # if no email entity was recognised, prompt the user to enter a valid
+        # email and go back a turn in the conversation to ensure future
+        # predictions aren't affected
+        if not email:
+            dispatcher.utter_message("We need your email, please enter a valid one.")
+            return [UserUtteranceReverted()]
+
+        return [SlotSet('email', email)]
