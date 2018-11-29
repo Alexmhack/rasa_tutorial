a1d424622bb5af73e0271fba4c922ad335fa558e
alexHooper <alexmohack@gmail.com>
2018-11-29 11:22:58 +0530

    change the post url

--- core_jwt/rest_post.py
+++ core_jwt/rest_post.py
@@ -1,17 +1,28 @@
 import requests
+import jwt
 
-url = 'http://localhost:5005/webhooks/rest/webhook'
+# url = 'http://localhost:5005/webhooks/rest/webhook'
+url = 'http://localhost:5005/conversations/rasa/respond'
+
+payload = {
+	'username': 'rasa',
+	'role': 'user',
+}
+
+token = jwt.encode(payload, 'thisismysecret', algorithm='HS256')
+token = token.decode("utf-8")
+print(token)
 
 while True:
 	message = input(">> ")
 	if message == 'stop':
 		break
 	data = {
-		"sender": "Rasa",
-		"message": f"{message}"
+		"sender_id": "rasa",
+		"query": f"{message}"
 	}
 	try:
-		r = requests.post(url, json=data)
+		r = requests.post(url, json=data, headers={'Authorization': 'Bearer ' + f'{token}'})
 		print(r.status_code)
 		print(r.json())
 	except Exception as e:
